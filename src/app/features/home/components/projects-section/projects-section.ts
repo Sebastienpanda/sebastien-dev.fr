d74acae63881771f7d22578b0e0b10c70c763775
Sebastienpanda <sebastienprodev@gmail.com>
2025-07-13 12:44:04 +0200

    feat: add two projects

--- src/app/features/home/components/projects-section/projects-section.ts
+++ src/app/features/home/components/projects-section/projects-section.ts
@@ -12,6 +12,23 @@ import { ProjectCard } from './components/project-card';
 export class ProjectsSection {
     readonly projects = [
         {
+            title: 'Puissance 4 - Frontend Mentor',
+            tags: ['Frontend', 'HTML', 'CSS', 'Backend', 'Angular', 'Supabase', 'NestJS'],
+            image: '/images/puissance-4.jpg',
+            description: `• Création d'un jeu de Puissance 4 en ligne\n• Intégration d'une API pour la gestion des parties\n• Authentification et sauvegarde des scores\n`,
+            link: 'https://puissance-4.sebastien-dev.fr/',
+            github: '',
+        },
+        {
+            title: 'Extension Manager - Frontend Mentor',
+            tags: ['Frontend', 'HTML', 'SCSS', 'Angular'],
+            image: '/images/extensions-manager.jpg',
+            description: `
+            • Création d'un gestionnaire d'extensions pour navigateur\n• Intégration d'une interface utilisateur responsive\n• Utilisation de SCSS pour le style\n`,
+            link: 'https://extension-manager.sebastien-dev.fr/',
+            github: 'https://github.com/Sebastienpanda/extension-manager',
+        },
+        {
             title: 'La Petite Boutique d\'Éphée',
             tags: ['Frontend', 'Backend', 'Angular', 'Supabase', 'TailwindCSS', 'Stripe', 'DaisyUI'],
             image: '/images/petiteboutiquedephee.png',
